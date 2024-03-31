f28338d5347cad0f0128833a1dfdf0ac5f285f8d
cettareswara <13521133@std.stei.itb.ac.id>
2024-04-01 01:58:33 +0700

    update: layer, node

--- src/layer.py
+++ src/layer.py
@@ -8,7 +8,7 @@ module_dir = os.path.join(current_dir, '..', 'function')
 sys.path.append(module_dir)
 
 class Layer:
-    def __init__(self, bias, weight, name, neuron):
+    def __init__(self, weight, name, activation, n_neuron):
         activation_function = {
             "linear" : linear,
             "relu" : relu,
@@ -16,16 +16,40 @@ class Layer:
             "softmax" : softmax,
             "None" : None
         }
-        self.bias = bias
+        self.bias = 1
         self.weight = weight
         self.name = name
-        self.neuron = neuron
-        self.activation_function = activation_function[name]
+        self.n_neuron = n_neuron
+        self.activation_function = activation_function[activation]
         self.value = []
         self.net = []
         self.node = []
         self.generate()
     
     def generate(self):
-        for i in range(self.neuron):
-            self.node.append(Node(self.bias[i], self.weight[i], self.activation_function, self.name))
+        for i in range(self.n_neuron):
+            self.node.append(Node(1, self.weight[i], self.activation_function, i+1))
+
+    def activate_layer(self, input):
+        for i in range(self.n_neuron):
+            self.node.append(self.node[i].calculate_net(input))
+            self.value.append(self.node[i].activate_neuron())
+    
+    def __str__(self):
+        print('Layer',self.name)
+        print('')
+        for i in range(self.n_neuron):
+            print(self.node[i])
+        return ''
+    
+if __name__ == "__main__":
+    Layer1 = Layer([
+                    [0.1, 0.2, 0.3],
+                    [0.4, -0.5, 0.6],
+                    [0.7, 0.8, -0.9]
+                   ], 1, 'relu', 3)
+    
+    Layer1.activate_layer([[-1.0, 0.5]])
+    print(Layer1)
+    print(Layer1.value)
+    
--- src/node.py
+++ src/node.py
@@ -10,10 +10,30 @@ class Node:
         self.value = 0
         
     def calculate_net(self, input):
-        self.net = np.dot(self.weight, input) + self.bias
+        net_array = np.concatenate(([1], input[0]),)
+        self.net = np.dot(self.weight, net_array)
+        return self.net
+        
         
     def activate_neuron(self, sum = None):
         if (self.name != "softmax"):
             self.value = self.activation_function(self.net)
         else:
             self.value = self.activation_function(self.net, sum)
+        return self.value
+    
+    def __repr__ (self):
+        print('Node ', self.name)
+        print('Weight: ',self.weight)
+        #print(self.net)
+        print('Value: ', self.value)
+        return ''
+
+if __name__ == "__main__":
+    from activation import relu
+    Node1 = Node(1, 
+                [0.1, 0.2, 0.3]
+                , relu, 2)
+    Node1.calculate_net([[-1.0, 0.5]])
+    Node1.activate_neuron()
+    print(Node1)
